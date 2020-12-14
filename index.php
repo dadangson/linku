04628d0d04e7bfc353ae715082dd2e3a94146800
Dadang Suhendar <dadangdheh@gmail.com>
2020-12-14 22:43:51 +0700

    menambahkan repositories (commit on master)

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="asset/fontawesome-free-5.14.0-web">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<!-- header -->
+	<div class="header">
+		<div class="container">
+			<div class="header-logo"><a href="index.php">LinKu</a></div>
+			<div class="header-list">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="view/list.php">List</a></li>
+					<li><a href="view/help.php">Help</a></li>
+				</ul>
+			</div>
+			<div class="header-search">
+				<div class="search"></div>
+				<div class="login"><a href="">Login</a></div>
+			</div>
+		</div>
+	</div>
+
+	<!-- content -->
+	<div class="content">
+		<div class="container">
+			<!-- content main -->
+			<div class="content-main">
+				<div class="content-item">
+					<div class="content-title">nama sudah ditambahkan</div>
+					<small>7 desember 2020</small>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+				</div>
+				<!-- pagination -->
+				<div class="pagination"></div>
+			</div>
+			<!-- content asaid -->
+			<div class="content-aside">
+				<div class="aside-populer"></div>
+				<div class="aside-letter"></div>
+			</div>
+		</div>
+	</div>
+
+	<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="footer-logo">LinKu</div>
+			<div class="footer-title"><small>copy right made heart by Linku.</small></div>
+		</div>
+	</div>
+
+</body>
+</html>
