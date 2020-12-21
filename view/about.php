54c994fb3a02db942c5458ccd72ecce6f6a0d125
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-21 11:10:00 +0700

    Menambahkan isi about.php

--- view/about.php
+++ view/about.php
@@ -1,1 +1,86 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Detail</title>
+	<link rel="stylesheet" type="text/css" href="../asset/fontawesome-free-5.14.0-web">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+
+	<!-- header -->
+	<div class="header">
+		<div class="container">
+			<div class="header-logo"><a href="../index.php">LinKu</a></div>
+			<div class="header-list">
+				<ul>
+					<li><a href="../index.php">Home</a></li>
+					<li><a href="list.php">List</a></li>
+					<li><a href="about.php">About</a></li>
+				</ul>
+			</div>
+			<div class="header-search">
+				<div class="search"></div>
+				<div class="login"><a href="">Login</a></div>
+			</div>
+		</div>
+	</div>
+
+	<!-- content -->
+	<div class="contents">
+		<div class="container">
+		  <div class="about-wrapper">
+		    <div class="about">
+		      <h3 class="about-title">About LinKu</>
+		      <p>LinKu is website yang menyediakan musik untuk beli atau download. Kami tidak menyediakan musik yang legal. Untuk mensupport karyanya belilah di situs" web penyedia yang berbayar.</p>
+		    </div>
+		    <div class="help">
+		      <h3 class="about-title">How to download</h3>
+		      <ol>
+		        <li>Select your song to buy or download</li>
+		        <li>Click button Buy or Download</li>
+		        <li>If you buy the song, you redirect link amazone or etc, and follow the step by step they are website</li>
+		        <li>If you only download, you redirect to safelink or shrlink</li>
+		        <li>Lewati dengan mengikuti perintah tersebut</li>
+		        <li>Anda akan masuk ke web penyedia download</li>
+		        <li>Klik pada tombol download dan tunggu sampai selesai</li>
+		        <li>Jika terdapat iklan, balik ke halaman sebelumnya..</li>
+		      </ol>
+		    </div>
+		    <div class="contact">
+		      <h3 class="about-title">Contact LinKu</h3>
+		      <form action="">
+		        <div>
+		          <label for="name">Name</label>
+		          <input type="text" name="name" id="name" placeholder="Name" />
+		        </div>
+		        <div>
+		          <label for="email">Email</label>
+		          <input type="email" name="email" id="email" placeholder="Email" />
+		        </div>
+		        <div>
+		          <label for="message">Message</label>
+		          <textarea name="message" id=message rows="8" cols="40"></textarea>
+		        </div>
+		        <div>
+		          <input type="submit" name="submit" id="submit" value="Send" />
+		        </div>
+		      </form>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+	<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="footer-logo">LinKu</div>
+			<div class="footer-title"><small>copy right made heart by Linku.</small></div>
+		</div>
+	</div>
+
+</body>
+</html>
