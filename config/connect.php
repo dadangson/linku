ce28c03b03c377ccf8160b0a2682db97606b9350
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-23 19:03:39 +0700

    Membuat koneksi di connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,1 +1,15 @@
+<?php
 
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "linku";
+
+$connect = mysqli_connect($dbhost,$dbuser, $dbpass, $dbname);
+if (mysqli_connect_errno()) {
+  echo "Not connection : ".mysqli_connect_error();
+} else {
+  echo "Connection success..";
+}
+
+?>
