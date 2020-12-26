ae792a21c42867a7c5953961a70b908a76c97a2f
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-26 21:40:42 +0700

    Menambahkan code di process_delete.php

--- process/process_delete.php
+++ process/process_delete.php
@@ -1,1 +1,22 @@
+<?php
+require '../config/functions.php';
 
+$id = $_GET['id'];
+
+if( delete($id) > 0 ) {
+  echo "
+      <script>
+        alert('Data berhasil dihapus.');
+        document.location.href = 'dashboard.php';
+      </script>
+  ";
+} else {
+  echo "
+      <script>
+        alert('Data gagal dihapus.!');
+        document.location.href = 'database.php';
+      </script>
+  ";
+}
+
+?>
