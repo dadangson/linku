af493af98b632d37b249ef03e83d2052c6306224
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-27 07:00:04 +0700

    Menambah function upload di functions.php

--- config/functions.php
+++ config/functions.php
@@ -27,12 +27,64 @@ function input($data) {
   $link2 = htmlspecialchars($data['link2']);
   $tags = htmlspecialchars($data['tags']);
   
+  // upload image
+  $image = upload();
+  if( !$image ) {
+      return false;
+  }
+  
   $query = " INSERT INTO items
              VALUES
-            ('', '$title', '$actrist', '$type', '$release', '$bit_rate', '$size', '$buy', '$link1', '$link2', '$tags')";
+            ('', '$title', '$actrist', '$type', '$release', '$bit_rate', '$size', '$buy', '$link1', '$link2', '$tags', '$image')";
   mysqli_query($conn, $query);
   
   return mysqli_affected_rows($conn);
+}
+
+function upload() {
+  $nameFile = $_FILES['image']['name'];
+  $sizeFile = $_FILES['image']['size'];
+  $error = $_FILES['image']['error']
+  $tmpName = $_FILES['image']['tmp_name'];
+  
+  // check uploaded file
+  if( $image === 4 ){
+    echo "
+        <script>
+          alert('Pilih gambar terlebih dahulu!');
+        </script>
+    ";
+    return false;
+  }
+  // check extention file image
+  $extentionImageValid = ('jpg', 'jpeg', 'png');
+  $extentionImage = explode('.', $nameFile);
+  $extentionImage = strtolower(end($extentionImage));
+  if( !in_array($extentionImage, $extentionImageValid) ) {
+    echo "
+        <script>
+          alert('Yang anda upload bukan gambar!');
+        </script>
+    ";
+    return false;
+  }
+  // check size file image
+  if( $sizeFile > 2000000 ) {
+    echo "
+        <script>
+          alert('Ukuran gambar terlalu besar!');
+        </script>
+    ";
+    return false;
+  }
+  // file image uploaded
+  // generate new name file
+  $newNameFile = uniqid();
+  $newNameFile .= '.';
+  $newNameFile .= $extentionImage;
+  move_uploaded_file($tmpName, '../assets/image'.$newNameFile);
+  
+  return $newNameFile;
 }
 
 function delete($id) {
@@ -57,6 +109,14 @@ function update($data) {
   $link1 = htmlspecialchars($data['link1']);
   $link2 = htmlspecialchars($data['link2']);
   $tags = htmlspecialchars($data['tags']);
+  $imageOld = $data['imageOld'];
+  
+  // check upload image
+  if( $_FILES['image']['error'] === 4 ) {
+    $image = $imageOld;
+  } else {
+    $image = upload();
+  }
   
   $query = " UPDATE items SET
               title = '$title',
@@ -68,7 +128,8 @@ function update($data) {
               buy = '$buy',
               link1 = '$link1',
               link2 = '$link2',
-              tags = '$tags' 
+              tags = '$tags',
+              image = '$image'
             WHERE id = $id";
   mysqli_query($conn, $query);
   
