b42990c08b811d2243fd9597cb630c2c988d6944
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-27 07:17:15 +0700

    Menambahkan form input di input.php

--- dashboard/input.php
+++ dashboard/input.php
@@ -39,7 +39,102 @@
 		<div class="content">
 			<div class="content-title">Ini title</div>
 			<div>
-			  <form action="../process/process_input">
+			  <form action="../process/process_input" method="post" enctype="multipart/form-data">
+			    <table>
+			      <tr>
+			        <td>
+			          <label for="title">Title</label>
+			        </td>
+			        <td>
+			          <input type="text" name="title" id="title">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="actrist">Actrist</label>
+			        </td>
+			        <td>
+			          <input type="text" name="actrist" id="actrist">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="type">Type</label>
+			        </td>
+			        <td>
+			          <input type="text" name="type" id="type">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="release">Release</label>
+			        </td>
+			        <td>
+			          <input type="text" name="release" id="release">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="bit_rate">Bit Rate</label>
+			        </td>
+			        <td>
+			          <input type="text" name="bit_rate" id="bit_rate">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="size">Size</label>
+			        </td>
+			        <td>
+			          <input type="text" name="size" id="size">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="buy">Buy</label>
+			        </td>
+			        <td>
+			          <input type="text" name="buy" id="buy">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="link1">Link1</label>
+			        </td>
+			        <td>
+			          <input type="text" name="link1" id="link1">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="link2">Link2</label>
+			        </td>
+			        <td>
+			          <input type="text" name="link2" id="link2">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="tags">Tags</label>
+			        </td>
+			        <td>
+			          <input type="checkbox" name="tags" id="title" value="single">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <label for="image">image</label>
+			        </td>
+			        <td>
+			          <input type="file" name="image" id="image">
+			        </td>
+			      </tr>
+			      <tr>
+			        <td>
+			          <button type="submit" name="submit">Send</button>
+			        </td>
+			      </tr>
+			    </table>
 			    
 			  </form>
 			</div>
