496be2b22c190351a2b47a901cfbe54fa3d5d8b1
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-28 05:59:41 +0700

    Membuat code di process_register.php

--- process/process_register.php
+++ process/process_register.php
@@ -1,1 +1,18 @@
+<?php
+require '../config/functions.php';
 
+if( isset($_POST['register']) ) {
+  if( register($_POST) > 0 ) {
+    echo "
+        <script>
+            alert('Registrasi berhasil');
+            document.location.href = 'login.php';
+        </script>
+    ";
+  } else {
+    echo mysqli_error($conn);
+  }
+}
+
+
+?>
