e94d07b72d2438fee5d0a0e0c9fbdbe1829db468
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-27 08:26:39 +0700

    Membuat code process_update.php

--- process/process_update.php
+++ process/process_update.php
@@ -1,1 +1,24 @@
 
+<?php
+require '../config/functions.php';
+
+if( isset($_POST["update"]) ) {
+  //cek data inputan
+  if( update($_POST) > 0 ) {
+    echo "
+      <script>
+        alert('Data berhasil diubah');
+        document.location.href = 'database.php';
+      </script>
+    ";
+  } else {
+    echo "
+      <script>
+        alert('Data gagal diubah');
+        document.location.href = 'input.php';
+      </script>
+    ";
+  }
+}
+
+?>
