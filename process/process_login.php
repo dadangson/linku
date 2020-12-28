ce7d0bc00add0a2d0d0abf596535784a2c98689f
dadangson <68373509+dadangson@users.noreply.github.com>
2020-12-28 10:25:20 +0700

    Menambahkan logic login di process_login.php

--- process/process_login.php
+++ process/process_login.php
@@ -1,1 +1,24 @@
+<?php
+require '../config/functions.php';
 
+if( isset($_POST['login']) ) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  
+  $result = mysqli_query( $conn, " SELECT * FROM users WHERE username = '$username'" );
+  
+  // check username
+  if( mysqli_num_rows($result) ===1 ) {
+    
+    // check password
+    $row = mysqli_fetch_assoc($result);
+    if( password_verify($password, $row['password']) ) {
+      header("location:../index.php");
+      exit;
+    }
+  }
+  $error = true;
+}
+
+
+?>
